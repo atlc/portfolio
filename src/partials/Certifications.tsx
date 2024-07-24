3b9bd2aecc0a6d3fba4271817a814b52e0bc0d6b
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-07-24 15:17:08 -0500

    Updating cert language and links

--- src/partials/Certifications.tsx
+++ src/partials/Certifications.tsx
@@ -28,13 +28,15 @@ const Certifications: React.FC = () => {
 function AWS() {
     return (
         <div style={{ border: "2px solid", borderRadius: "12px", margin: "2%", padding: "20px" }}>
-            <h2>Amazon</h2>
+            <h2>AWS</h2>
             <ul>
                 <li>
-                    <h4>AWS Certified Cloud Practitioner</h4>
+                    <h4>
+                        <a href="https://www.credly.com/badges/9043eb59-5ee7-49f6-9072-900a2a954ed8">Certified Cloud Practitioner</a>
+                    </h4>
                 </li>
                 <li>
-                    <h4>AWS Certified Developer (expected completion August 2024)</h4>
+                    <h4>Certified Developer (expected completion August 2024)</h4>
                 </li>
             </ul>
         </div>
@@ -82,25 +84,23 @@ function Microsoft() {
                 <li>
                     <h4>
                         <a href="https://learn.microsoft.com/api/credentials/share/en-us/AndrewCartwright-0770/C0AF3E712D6EC64F?sharingId=27FBD064B975A40C">
-                            Azure Fundamentals (AZ-900)
+                            Azure Fundamentals
                         </a>
                     </h4>
                 </li>
                 <li>
                     <h4>
-                        <a href="https://www.youracclaim.com/badges/ddfa48f5-981f-445c-a16b-0adfecd18f6d/public_url">
-                            MTA Database Administration Fundamentals
-                        </a>
+                        <a href="https://www.youracclaim.com/badges/ddfa48f5-981f-445c-a16b-0adfecd18f6d/public_url">Database Administration Fundamentals</a>
                     </h4>
                 </li>
                 <li>
                     <h4>
-                        <a href="https://www.youracclaim.com/badges/2bdeff35-cf12-4228-af03-e3485a0c60d8/public_url">MTA Software Development Fundamentals</a>
+                        <a href="https://www.youracclaim.com/badges/2bdeff35-cf12-4228-af03-e3485a0c60d8/public_url">Software Development Fundamentals</a>
                     </h4>
                 </li>
                 <li>
                     <h4>
-                        <a href="https://www.youracclaim.com/badges/91a09b81-4a76-448a-be2c-7ea49551e31b">MTA Security Fundamentals</a>
+                        <a href="https://www.youracclaim.com/badges/91a09b81-4a76-448a-be2c-7ea49551e31b">Security Fundamentals</a>
                     </h4>
                 </li>
             </ul>
